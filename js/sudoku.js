f9f3e006b25c9ea2a1032a327d1d5f4b549e8938
Jaideep25 <jaideepch@outlook.com>
2021-02-16 18:37:54 +0530

    Initial sharing of project

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,258 +1,379 @@
 /**
- * Sudoku
- * Author: Varun Varada
+ * Sudoku generator + Sudoku grid classes
+ * 
+ * @author Jani Hartikainen <firstname at codeutopia net>
  */
-
-(function ( window ) {
-
-// Enable strict mode
-'use strict';
 
 /**
- * Instantiates a new Sudoku board
+ * Modified and extended by Raymond May Jr.
+ *
  */
-var Sudoku = function() {
 
-    /**
-     * Holds the size (n) of the current board, where the board is (n x n)
-     *
-     * @type {Number}
-     */
-    this.boardSize = 0;
+/** added by rm
+ * indexOf added in ecma 1.6 - not implemented in ie 8 or lower
+ * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/IndexOf
+ */
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+        "use strict";
+        if (this == null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 0) {
+            n = Number(arguments[1]);
+            if (n != n) { // shortcut for verifying if it's NaN
+                n = 0;
+            } else if (n != 0 && n != Infinity && n != -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
+    }
+}
 
-    /**
-     * Holds the number of currently filled cells on the board
-     *
-     * @type {Number}
-     */
-    this.filledCells = 0;
 
-    /**
-     * Multidimensional array that holds the initial board state
-     *
-     * @type {Array}
-     */
-    this.initialBoard = [];
+if(!CU)
+	var CU = { };
 
-    /**
-     * Multidimensional array that holds the current board state
-     *
-     * @type {Array}
-     */
-    this.board = [];
+CU.Sudoku = {
+	/**
+	 * Generate a CU.sudoku.Grid
+	 * @return CU.sudoku.Grid a new random sudoku puzzle
+	 */
+	generate: function() {
+		var grid = new CU.sudoku.Grid();
+	
+		//We need to keep track of all numbers tried in every cell
+		var cellNumbers = [];
+		for(var i = 0; i < 81; i++)
+		{
+			cellNumbers[i] = [1,2,3,4,5,6,7,8,9];
+		}
 
-    /**
-     * Multidimensional array that holds the current board's solution
-     *
-     * @type {Array}
-     */
-    this.solution = [];
-
+		for(var i = 0; i < 81; i++)
+		{	
+			var found = false;
+			var row = Math.floor(i / 9);
+			var col = i - (row * 9);
+			
+			while(cellNumbers[i].length > 0)
+			{
+				//Pick a random number to try
+				var rnd = Math.floor(Math.random() * cellNumbers[i].length);
+				var num = cellNumbers[i].splice(rnd, 1)[0];
+				
+				grid.setValue(col, row, num);
+				
+				if(!grid.cellConflicts(col, row)) 
+				{
+					found = true;						
+					break;
+				}
+				else
+				{
+					grid.setValue(col, row, 0);
+					found = false;
+					continue;
+				}
+			}
+			
+			//If a possible number was not found, backtrack			
+			if(!found)
+			{
+				//After backtracking we can try all numbers here again
+				cellNumbers[i] = [1,2,3,4,5,6,7,8,9];
+				
+				//Reduce by two, since the loop increments by one
+				i -= 2;
+			}
+		}
+			
+		return grid;
+	},
+	
+	/**
+	 * Clear N cells from the sudoku grid randomly
+	 * @param {CU.sudoku.Grid} grid
+	 * @param {Number} amount
+	 */
+	cull: function(grid, amount) {
+		var cells = [];
+		for(var i = 0; i < 81; i++)
+			cells.push(i);
+			
+		for(var i = 0; i < amount; i++)
+		{
+			var rnd = Math.floor(Math.random() * cells.length);
+			var value = cells.splice(rnd, 1);
+			var row = Math.floor(value / 9);
+			var col = value - (row * 9);
+			
+			grid.setValue(col, row, 0);	
+		}
+	}	
 };
 
-$.extend( true, Sudoku.prototype, {
+CU.sudoku = { };
 
-    /**
-     * Gets the current board
-     *
-     * @return {Array[]} The current board
-     */
-    getBoard: function() {
-        return this.board;
-    },
+/**
+ * A class for representing sudoku puzzle grids
+ * @constructor
+ */
+CU.sudoku.Grid = function() {
+	this.rows = [];
+	for(var row = 0; row < 9; row++)
+	{
+		var cols = [];
+		for(var col = 0; col < 9; col++)
+			cols[col] = 0;
+			
+		this.rows[row] = cols;
+	}
+};
 
-    /**
-     * Gets the current board's solution
-     *
-     * @return {Array[]} The current board's solution
-     */
-    getSolution: function() {
-        return this.solution;
-    },
+CU.sudoku.Grid.prototype = {
+	rows: [],
+	
+	/**
+	 * Return value of a col,row in the grid
+	 * @method
+	 * @param {Number} col
+	 * @param {Number} row
+	 * @return {Number} 0 to 9, 0 meaning empty
+	 */
+	getValue: function(col, row) {
+		return this.rows[row][col];
+	},
+	
+	/**
+	 * Set value of col,row in the grid.
+	 * @method
+	 * @param {Number} column
+	 * @param {Number} row
+	 * @param {Number} value 0 to 9, 0 meaning empty
+	 */
+	setValue: function(column, row, value) {
+		this.rows[row][column] = value;
+	},
 
-    /**
-     * Generates a new board
-     * Note: Gives the same board every time for the moment (have to implement a real board generator).
-     *
-     * @return {Array[]} The generated board
-     */
-    generate: function() {
-        var i;
+	/**
+	 * Does a specific cell conflict with another?
+	 * @method
+	 * @param {Number} column
+	 * @param {Number} row
+	 * @return {Boolean}
+	 */
+	cellConflicts: function(column, row) {
+		var value = this.rows[row][column];
+		
+		if(value == 0)
+			return false;
+			
+		for(var i = 0; i < 9; i++)
+		{
+			if(i != row && this.rows[i][column] == value) 
+			{
+				return true;
+			}
 
-        // Set board size
-        this.boardSize = 9;
+			if(i != column && this.rows[row][i] == value)
+			{
+				return true;
+			}
+		}
 
-        // Initialize board
-        this.board = [
-            '5 3 0 ' + '0 7 0 ' + '0 0 0',
-            '6 0 0 ' + '1 9 5 ' + '0 0 0',
-            '0 9 8 ' + '0 0 0 ' + '0 6 0',
+		//At this point, everything else is checked as valid except the 3x3 grid
+		return !this._miniGridValid(column, row);
+	},
+	
+	/**
+	 * Checks if the inner 3x3 grid a cell resides in is valid
+	 * @method
+	 * @private
+	 * @param {Number} column
+	 * @param {Number} row
+	 * @return {Boolean}
+	 */
+	_miniGridValid: function(column, row) {		
+		//Determine 3x3 grid position
+		var mgX = Math.floor(column / 3);
+		var mgY = Math.floor(row / 3);
+		
+		var startCol = mgX * 3;
+		var startRow = mgY * 3;
+		
+		var endCol = (mgX + 1) * 3;
+		var endRow = (mgY + 1) * 3;
+		
+		var numbers = [];
+		for(var r = startRow; r < endRow; r++)
+		{
+			for(var c = startCol; c < endCol; c++)
+			{
+				var value = this.rows[r][c];
+				if(value == 0)
+					continue;
+					
+				if(numbers.indexOf(value) !== -1) /* mod by rm */
+					return false;
+					
+				numbers.push(value);			
+			}
+		}
+		
+		return true;
+	},
 
-            '8 0 0 ' + '0 6 0 ' + '0 0 3',
-            '4 0 0 ' + '8 0 3 ' + '0 0 1',
-            '7 0 0 ' + '0 2 0 ' + '0 0 6',
+	/** added by rm
+	 * Is a cell's value valid
+	 * @method
+	 * @param {Number} column
+	 * @param {Number} row
+	 * @return {Boolean}
+	 */
+	cellValid: function(column, row){
+	    return !(this.inColRow(column, row) || this.inMiniGrid(column, row));
+        },
 
-            '0 6 0 ' + '0 0 0 ' + '2 8 0',
-            '0 0 0 ' + '4 1 9 ' + '0 0 5',
-            '0 0 0 ' + '0 8 0 ' + '0 7 9'
-        ];
-        for ( i = 0; i < this.boardSize; i++ ) {
-            this.board[i] = this.board[i].split( ' ' );
-        };
+	/** added by rm
+	 * Is grid solved? Returns false on first invalid cell
+	 * For checking status.
+	 * @method
+	 * @return {Boolean}
+	 */
+	gridSolved: function(){
+	    for(var r = 0; r < 9; r++){
+		for(var c = 0; c < 9; c++){
+		    if(this.getValue(c,r) == 0)
+			return false;
+		    if(!this.cellValid(c,r))
+			return false;
+		}
+	    }
+	    return true;
+        },
+	
+	/** added by rm
+         * Does a specific cell's value exist in the row or column?
+         * @method
+         * @param {Number} column
+         * @param {Number} row
+         * @return {Boolean}
+         */
+        inColRow: function(column, row) {
+	    var value = this.rows[row][column];
 
-        // Set number of filled cells
-        this.filledCells = 30;
+	    if(value == 0)
+		return false;
 
-        // Keep a copy of the initial board
-        this.initialBoard = $.extend( true, [], this.board );
+	    for(var i = 0; i < 9; i++)
+		{
+		    if(i != row && this.rows[i][column] == value)
+			{
+			    return true;
+			}
+		    
+		    if(i != column && this.rows[row][i] == value)
+			{
+			    return true;
+			}
+		}
+	    return false;
+        },
 
-        // Initialize corresponding solution
-        this.solution = [
-            '5 3 4 ' + '6 7 8 ' + '9 1 2',
-            '6 7 2 ' + '1 9 5 ' + '3 4 8',
-            '1 9 8 ' + '3 4 2 ' + '5 6 7',
+	/** added by rm
+	 * Return whether cell's value exists in mini grid
+	 * @method
+	 * @return {Boolean}
+	 */
+	inMiniGrid: function(column, row) {
+	    //Determine 3x3 grid position                                                                
+	    var mgX = Math.floor(column / 3);
+	    var mgY = Math.floor(row / 3);
 
-            '8 5 9 ' + '7 6 1 ' + '4 2 3',
-            '4 2 6 ' + '8 5 3 ' + '7 9 1',
-            '7 1 3 ' + '9 2 4 ' + '8 5 6',
+	    var startCol = mgX * 3;
+	    var startRow = mgY * 3;
 
-            '9 6 1 ' + '5 3 7 ' + '2 8 4',
-            '2 8 7 ' + '4 1 9 ' + '6 3 5',
-            '3 4 5 ' + '2 8 6 ' + '1 7 9'
-        ];
-        for ( i = 0; i < this.boardSize; i++ ) {
-            this.solution[i] = this.solution[i].split( ' ' );
-        };
+	    var endCol = (mgX + 1) * 3;
+	    var endRow = (mgY + 1) * 3;
 
-        return this.board;
-    },
+	    var value = this.rows[row][column];
+	    for(var r = startRow; r < endRow; r++){
+		for(var c = startCol; c < endCol; c++){
+		    if(this.rows[r][c] == 0 || r == row || c == column)
+			continue;
+		    
+		    if(this.rows[r][c] == value)
+			return true;
+		}
+	    }
+	    return false;
+        },
 
-    /**
-     * Checks whether the provided solution is correct
-     * Note: Assumes puzzles have unique solutions, which is true in 99.99% of the cases.
-     *
-     * @param  {Array}    board
-     * @return {Boolean}          Whether the provided solution is correct or not
-     */
-    isSolved: function() {
-        var i, j;
-
-        // Check whether all cells have been filled
-        if( this.filledCells !== Math.pow( this.boardSize, 2 ) ) {
-            return false;
-        }
-
-        for( i = 0; i < this.board.length; i++ ) {
-            // Verify values
-            // Note: Not using strict equals to because we want to ignore type inconsistencies.
-            for( j = 0; j < this.board[i].length; j++ ) {
-                // Empty value
-                if( this.board[i][j] == 0 ) {
-                    return false;
-                }
-
-                if( this.board[i][j] != this.solution[i][j] ) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    },
-
-    /**
-     * Updates the board at specified position with number.
-     *
-     * @param  {Object} position The position to update
-     * @param  {Number} number   The number to update the position with
-     */
-    update: function ( position, number ) {
-        // Update filled cells count
-        if( this.board[position.row][position.column] == 0 && number != 0 ) {
-            ++this.filledCells;
-        } else if( this.board[position.row][position.column] != 0 && number == 0 ) {
-            --this.filledCells;
-        }
-
-        this.board[position.row][position.column] = number;
-    },
-
-    /**
-     * Clears a position on the board
-     *
-     * @param  {Object} position The position to clear
-     */
-    clear: function ( position ) {
-        this.update( position, 0 );
-    },
-
-    /**
-     * Resets the board to its initial state and returns it
-     *
-     * @return {Array[]} The reset board
-     */
-    reset: function() {
-        this.board = $.extend( true, [], this.initialBoard );
-        return this.board;
-    },
-
-    /**
-     * Gets a list of valid numbers that can be played in a certain board position
-     *
-     * @param  {Object} position The position on the board
-     * @return {Array}          An array of valid numbers
-     */
-    getValidNumbers: function ( position ) {
-        var i, j, index, validNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-        // Check row
-        for( i = 0; i < this.board[position.row].length; i++ ) {
-            if( this.board[position.row][i] != 0 ) {
-                // Remove number that's been used from the valid numbers
-                // Note: Converting array items to numbers to make types compatible
-                index = validNumbers.betterIndexOf( +this.board[position.row][i] );
-                if( index !== -1 ) {
-                    validNumbers.splice( index, 1 );
-                }
-            }
-        }
-
-        // Check column
-        for( i = 0; i < this.board.length; i++ ) {
-            if( this.board[i][position.column] != 0 ) {
-                // Remove number that's been used from the valid numbers
-                // Note: Converting array items to numbers to make types compatible
-                index = validNumbers.betterIndexOf( +this.board[i][position.column] );
-                if( index !== -1 ) {
-                    validNumbers.splice( index, 1 );
-                }
-            }
-        }
-
-        // Check block
-        var beginRow = position.row - (position.row % 3),
-            beginCol = position.column - (position.column % 3),
-            endRow = beginRow + 2,
-            endCol = beginCol + 2;
-        for( i = beginRow; i <= endRow; i++ ) {
-            for( j = beginCol; j <= endCol; j++ ) {
-                if( this.board[i][j] != 0 ) {
-                    // Remove number that's been used from the valid numbers
-                    // Note: Converting array items to numbers to make types compatible
-                    index = validNumbers.betterIndexOf( +this.board[i][j] );
-                    if( index !== -1 ) {
-                        validNumbers.splice( index, 1 );
-                    }
-                }
-            }
-        }
-
-        return validNumbers;
-    }
-
-});
-
-// Expose the constructor to the global scope
-window.Sudoku = Sudoku;
-
-})( window );
+	/**
+	 * Return a string representation of the grid.
+	 * @method
+	 * @return {String}
+	 */
+	toString: function() {
+		var str = '';
+		for(var i = 0; i < 9; i++)
+		{
+			str += this.rows[i].join(' ') + "\r\n";
+		}
+		
+		return str;
+	},
+	
+	/**
+	 * Return the puzzle as an array, for example for saving
+	 * @method
+	 * @return {Array}
+	 */
+	toArray: function() {
+		var cells = [];
+		for(var row = 0; row < 9; row++)
+		{
+			for(var col = 0; col < 9; col++)
+				cells.push(this.rows[row][col]);
+		}
+		
+		return cells;
+	},
+	
+	/**
+	 * Fill the puzzle from an array
+	 * @method
+	 * @param {Array} cells
+	 * @return {CU.sudoku.Grid}
+	 */
+	fromArray: function(cells) {
+		if(cells.length != 81)
+			throw new Error('Array length is not 81');
+			
+		for(var i = 0; i < 81; i++)
+		{
+			var row = Math.floor(i / 9);
+			var col = i - (row * 9);
+			
+			this.rows[row][col] = cells[i];
+		}
+		
+		return this;
+	}
+};
